88defa4b891e7fb905cd8ef0487a10be34a99274
HuangYuTse <eric19961231@gmail.com>
2020-09-19 18:55:36 +0800

    Update GM1602lib.h

--- GM1602lib.h
+++ GM1602lib.h
@@ -1,8 +1,4 @@
-/*
-  Morse.h - Library for flashing Morse code.
-  Created by David A. Mellis, November 2, 2007.
-  Released into the public domain.
-*/
+
 
 #ifndef GM1602lib_h
 #define GM1602lib_h
